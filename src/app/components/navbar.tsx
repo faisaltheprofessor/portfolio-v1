b8b10b1fc5cc7d6cd2d725a14b1ec19e65fddcde
Faisal Khan <mahboobshah.faisal@gmail.com>
2024-02-13 01:43:46 +0100

    logo

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,4 +1,6 @@
 import SolidButton from "./SolidButton";
+import logo from "../../../public/logo.svg";
+import Image from "next/image";
 const Navbar = () => {
   const navItmes = [
     { id: 1, section: "About" },
@@ -8,7 +10,9 @@ const Navbar = () => {
   ];
   return (
     <nav className="h-[42] w-full flex items-center justify-between stick">
-      <a href="#"></a>
+      <a href="#">
+        <Image src={logo} alt="logo" height={150} />
+      </a>
       <ul className="hidden md:flex items-center gap-x-8 text-light-slate">
         {navItmes.map((item) => (
           <li className="hover:text-green" key={item.id}>
