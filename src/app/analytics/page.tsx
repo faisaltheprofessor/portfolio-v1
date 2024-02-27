302af1336caf89e9adf32be26dbdb519011d7f39
Faisal Khan <faisalfazily@gmail.com>
2024-02-27 14:35:56 +0100

    remove comment

--- src/app/analytics/page.tsx
+++ src/app/analytics/page.tsx
@@ -36,12 +36,6 @@ const Page = async () => {
         />
       </div>
     </div>
-    // <main className="flex items-center  justify-center w-full  xl:w-[50%] h-screen mx-auto flex-col md:w-1/2 gap-y-4">
-    //   <h1 className={cn("text-7xl font-bold text-white", Calibre.className)}>
-    //     {" "}
-    //     <pre>{JSON.stringify(pageview)}</pre>
-    //   </h1>
-    // </main>
   );
 };
 export default Page;
